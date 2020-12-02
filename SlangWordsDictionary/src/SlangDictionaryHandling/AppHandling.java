8452c2e90ced41de732a9a4610b3099d1e983dbd
Phát Dương <69242061+beebapcay@users.noreply.github.com>
2020-12-02 17:06:04 +0700

    Console UI

--- SlangWordsDictionary/src/SlangDictionaryHandling/AppHandling.java
+++ SlangWordsDictionary/src/SlangDictionaryHandling/AppHandling.java
@@ -1,24 +1,173 @@
 package SlangDictionaryHandling;
 
 import java.util.Scanner;
+import java.io.IOException;
 
 import FileHandling.*;
 import SlangDictionaryHandling.SlangDictionary;
 
 public class AppHandling {
 
-    public static void main(String args[]) {
-
-        String file_data = "resources/slang.txt";
-        String file_original  = "resources/slang-original.txt";
-        SlangDictionary slangDic = new SlangDictionary(file_data, file_original);
-        slangDic.loadSlangWordData();
-
-//        Scanner myReader = new Scanner(System.in);
-//        System.out.print("Input: ");
-//        String key = myReader.next();
-
-        slangDic.showQuizSlangWord(2);
+    //Clears Screen in java
+    public static void clrscr(){
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("cls");
+        } catch (IOException | InterruptedException ex) {}
     }
 
+    public static boolean continueOption() {
+        Scanner myReader = new Scanner(System.in);
+        System.out.print("DO YOU WANT CONTINUE OPTION? (Y / N): ");
+        String confirm = myReader.nextLine();
+        if (confirm.equalsIgnoreCase("y") || confirm.equalsIgnoreCase("yes"))
+            return true;
+        else if (confirm.equalsIgnoreCase("n") || confirm.equalsIgnoreCase("no"))
+            return false;
+        else {
+            System.out.println("PLEASE INPUT Y / N");
+            return continueOption();
+        }
+    }
+
+    public static void main(String args[]) {
+        //Setup Data File
+        String file_data = "resources/slang.txt";
+        String file_original  = "resources/slang-original.txt";
+        String file_history = "resources/history.txt";
+
+        //Create And Load Data To SlangDictionary: slangDic
+        SlangDictionary slangDic = new SlangDictionary(file_data, file_original, file_history);
+        slangDic.loadSlangWordData();
+
+        Scanner myReader = new Scanner(System.in);
+        boolean con = true;
+
+        while (con) {
+            clrscr();
+
+            System.out.println("WELCOME TO SLANG WORD DICTIONARY");
+            System.out.println();
+
+            //Menu Option
+            System.out.println("OPTION 00: EXIT APPLICATION");
+            System.out.println("OPTION 01: FIND THE MEANING OF SLANG WORD");
+            System.out.println("OPTION 02: FIND THE SLANG WORD BY MEANING DEFINITION");
+            System.out.println("OPTION 03: SHOW HISTORY SLANG WORDS WERE SEARCHED");
+            System.out.println("OPTION 04: ADD NEW SLANG WORD");
+            System.out.println("OPTION 05: EDIT SLANG WORD");
+            System.out.println("OPTION 06: DELETE SLANG WORD");
+            System.out.println("OPTION 07: RESET TO DEFAULT DATA SLANG WORD");
+            System.out.println("OPTION 08: GIVE RANDOM SLANG WORD");
+            System.out.println("OPTION 09: GIVE QUIZ FIND MEANING OF SLANG WORD");
+            System.out.println("OPTION 10: GIVE QUIZ FIND SLANG WORD BY MEANING DEFINITION");
+            System.out.println();
+
+            System.out.print("PRESS OPTION YOU WANT: ");
+            int option = myReader.nextInt();
+            myReader.nextLine();
+
+            boolean op_con = true;
+            switch (option) {
+                case 0:
+                    System.out.println("\n** EXIT APPLICATION **\n");
+                    con = false;
+                    break;
+                case 1:
+                    System.out.println("\n** FIND THE MEANING OF SLANG WORD **\n");
+                    while (op_con) {
+                        System.out.print("ENTER YOUR SLANG WORD: ");
+                        String slangWord = myReader.nextLine();
+                        slangDic.recordHistory(slangWord);
+                        slangDic.searchDefinition(slangWord);
+                        op_con = continueOption();
+                    }
+                    break;
+                case 2:
+                    System.out.println("\n** FIND THE SLANG WORD BY MEANING DEFINITION **\n");
+                    op_con = true;
+                    while (op_con) {
+                        System.out.print("ENTER YOUR DEFINITION KEY WORD: ");
+                        String slangWord = myReader.nextLine();
+                        slangDic.searchSlangWord(slangWord);
+                        op_con = continueOption();
+                    }
+                    break;
+                case 3:
+                    System.out.println("\n** SHOW HISTORY SLANG WORDS WERE SEARCHED **\n");
+                    slangDic.showHistory();
+                    System.out.println("PRESS ENTER TO RETURN MENU");
+                    myReader.nextLine();
+                    break;
+                case 4:
+                    System.out.println("\n** ADD NEW SLANG WORD **\n");
+                    op_con = true;
+                    while (op_con) {
+                        System.out.print("ENTER NEW SLANG WORD KEY: ");
+                        String newSlangWord = myReader.nextLine();
+                        System.out.print("ENTER NEW SLANG DEFINITION: ");
+                        String newSlangDefinition = myReader.nextLine();
+                        slangDic.addSlangWord(newSlangWord, newSlangDefinition);
+                        op_con = continueOption();
+                    }
+                    break;
+                case 5:
+                    System.out.println("\n** EDIT SLANG WORD **\n");
+                    op_con = true;
+                    while (op_con) {
+                        System.out.print("ENTER SLANG WORD KEY: ");
+                        String slangWord = myReader.nextLine();
+                        slangDic.editSlangWord(slangWord);
+                        op_con = continueOption();
+                    }
+                    break;
+                case 6:
+                    System.out.println("\n** DELETE SLANG WORD **\n");
+                    op_con = true;
+                    while (op_con) {
+                        System.out.print("ENTER SLANG WORD KEY: ");
+                        String slangWord = myReader.nextLine();
+                        slangDic.deleteSlangWord(slangWord);
+                        op_con = continueOption();
+                    }
+                    break;
+                case 7:
+                    System.out.println("\n** RESET TO DEFAULT DATA SLANG WORD **\n");
+                    slangDic.resetToOriginalData();
+                    System.out.println("PRESS ENTER TO RETURN MENU");
+                    myReader.nextLine();
+                    break;
+                case 8:
+                    System.out.println("\n** GIVE RANDOM SLANG WORD **\n");
+                    op_con = true;
+                    while (op_con) {
+                        slangDic.randomShowSlangWord();
+                        op_con = continueOption();
+                    }
+                    break;
+                case 9:
+                    System.out.println("\n** GIVE QUIZ FIND MEANING OF SLANG WORD **\n");
+                    op_con = true;
+                    while (op_con) {
+                        slangDic.showQuizSlangWord(1);
+                        op_con = continueOption();
+                    }
+                    break;
+                case 10:
+                    System.out.println("\n** GIVE QUIZ FIND SLANG WORD BY MEANING DEFINITION **\n");
+                    op_con = true;
+                    while (op_con) {
+                        slangDic.showQuizSlangWord(2);
+                        op_con = continueOption();
+                    }
+                    break;
+                default:
+                    System.out.println("\n** PLEASE INPUT VALID VALUE **\n");
+                    con = true;
+                    break;
+            }
+        }
+    }
 }
--- SlangWordsDictionary/src/SlangDictionaryHandling/SlangDictionary.java
+++ SlangWordsDictionary/src/SlangDictionaryHandling/SlangDictionary.java
@@ -14,17 +14,20 @@ public class SlangDictionary {
     private HashMap<String, ArrayList<String>> m_slangDic;
     private String file_data;
     private String file_original;
+    private String file_history;
 
     public SlangDictionary() {
         m_slangDic = new HashMap<String, ArrayList<String>>();
         file_data = "";
         file_original = "";
+        file_history = "";
     }
 
-    public SlangDictionary(String file_data, String file_original) {
+    public SlangDictionary(String file_data, String file_original, String file_history) {
         m_slangDic = new HashMap<String, ArrayList<String>>();
         this.file_data = file_data;
         this.file_original = file_original;
+        this.file_history = file_history;
     }
 
     //Load slangDic From file_name
@@ -40,6 +43,23 @@ public class SlangDictionary {
                 String data = slangWord + "`" + slangDefinition;
                 FileHandling.writeToFileAppend(file_data, data);
             }
+        }
+    }
+
+    //Confirm Option
+    public static boolean confirmOption() {
+        Scanner myReader = new Scanner(System.in);
+        System.out.print("CONFIRM TO DO (Y / N):");
+        String confirm = myReader.nextLine();
+        if (confirm.equalsIgnoreCase("y") || confirm.equalsIgnoreCase("yes"))
+            return true;
+        else if (confirm.equalsIgnoreCase("n") || confirm.equalsIgnoreCase("no"))
+            return false;
+        else {
+            System.out.println("PLEASE INPUT Y / N");
+            System.out.println("PRESS ENTER CONFIRM AGAIN");
+            myReader.nextLine();
+            return confirmOption();
         }
     }
 
@@ -75,30 +95,39 @@ public class SlangDictionary {
     }
 
     //Option 03: Show History slangWord Search
-    public void recordHistory(String file_name, String data) {
-        FileHandling.writeToFileAppend(file_name, data);
+    public void recordHistory(String data) {
+        FileHandling.writeToFileAppend(file_history, data);
     }
 
-    public void showHistory(String file_name) {
+    public void showHistory() {
         System.out.println("THERE ARE HISTORIES SLANG WORD WERE SEARCHED");
-        FileHandling.readFromFile(file_name);
+        FileHandling.readFromFile(file_history);
     }
 
     //Option 04: Add New slangWord - slangDefinition. CaseSensitive
     public void addSlangWord(String newSlangWord, String newDefinition) {
+
+        //If slangDictionary Contains newSlangWord -> Overwrite Or Add New Meaning
        if (m_slangDic.containsKey(newSlangWord)) {
            ArrayList<String> slangDefinitions = m_slangDic.get(newSlangWord);
            System.out.println("ORIGINAL SLANG WORD " + newSlangWord + " HAS " + slangDefinitions.size() + " MEANINGS");
            for (int i = 0; i < slangDefinitions.size(); i++)
                System.out.println((i + 1) + ".\t" + slangDefinitions.get(i));
 
+           System.out.println("PRESS 0: TO EXIT ADD THIS SLANG WORD");
            System.out.println("PRESS 1: TO OVERWRITE ITS MEANINGS");
            System.out.println("PRESS 2: TO ADD NEW MEANING OF THIS SLANG WORD");
+
+           System.out.print("PRESS: ");
            Scanner myReader = new Scanner(System.in);
            int choose = myReader.nextInt();
            myReader.nextLine();
 
-           if (choose == 1) {
+           if (choose == 0) {
+               System.out.println("PRESS ENTER TO EXIT ADD THIS SLANG WORD");
+               myReader.nextLine();
+           }
+           else if (choose == 1) {
                slangDefinitions.clear();
                slangDefinitions.add(newDefinition);
                System.out.println("SUCCESSFULLY OVERWRITE");
@@ -109,12 +138,15 @@ public class SlangDictionary {
            }
            else {
                System.out.println("PLEASE PRESS ONLY 1 OR 2");
+               System.out.println("PLEASE ENTER TO INPUT AGAIN");
+               myReader.nextLine();
                addSlangWord(newSlangWord, newDefinition);
                return;
            }
            //Update File Data (Write Again)
            updateFileData();
        }
+       //If slangDictionary Does Not Contain newSlangWord -> Append
        else {
            ArrayList<String> newDefinitions = new ArrayList<>();
            newDefinitions.add(newDefinition);
@@ -127,76 +159,6 @@ public class SlangDictionary {
     }
 
     //Option 05: Edit slangWord
-    public void editSlangWord(String slangWord) {
-        if (m_slangDic.containsKey(slangWord)) {
-            ArrayList<String> slangDefinitions = m_slangDic.get(slangWord);
-            System.out.println("ORIGINAL SLANG WORD " + slangWord + " HAS " + slangDefinitions.size() + " MEANINGS");
-            for (int i = 0; i < slangDefinitions.size(); i++)
-                System.out.println((i + 1) + ".\t" + slangDefinitions.get(i));
-
-            System.out.println("PRESS 1: TO EDIT SLANG WORD");
-            System.out.println("PRESS 2: TO EDIT MEANING");
-            Scanner myReader = new Scanner(System.in);
-            int choose = myReader.nextInt();
-            myReader.nextLine();
-
-            if (choose == 1) {
-                System.out.print("INPUT NEW SLANG WORD: ");
-                String newSlangWord = myReader.nextLine();
-                System.out.print("CONFIRM TO CHANGE (Y / N):");
-                String confirm = myReader.nextLine();
-                if (confirm.equalsIgnoreCase("y") || confirm.equalsIgnoreCase("yes")) {
-                    changeSlangWordKey(slangWord, newSlangWord);
-                    updateFileData();
-                    System.out.println("SUCCESSFULLY CHANGE SLANG WORD");
-                }
-                else System.out.println("PLEASE INPUT Y / N");
-            }
-            else if (choose == 2) {
-                if (slangDefinitions.size() == 1)
-                    System.out.println("PRESS 1 TO EDIT DEFINITION");
-                else System.out.println("PRESS FROM 1 TO " + slangDefinitions.size() + " TO EDIT DEFINITION");
-                System.out.println("PRESS " + (slangDefinitions.size() + 1) + " TO ADD NEW DEFINITION");
-                int num = myReader.nextInt() - 1;
-                myReader.nextLine();
-                if (num < slangDefinitions.size()) {
-                    System.out.println("INPUT NEW DEFINITION: ");
-                    String newSlangDefinition= myReader.nextLine();
-                    System.out.println("CONFIRM TO CHANGE (Y / N):");
-                    String confirm = myReader.nextLine();
-                    if (confirm.equalsIgnoreCase("y") || confirm.equalsIgnoreCase("yes")) {
-                        changeDefinition(slangWord, num, newSlangDefinition);
-                        updateFileData();
-                        System.out.println("SUCCESSFULLY CHANGE SLANG DEFINITION");
-                    }
-                    else System.out.println("PLEASE INPUT Y / N");
-                }
-                else if (num == slangDefinitions.size()) {
-                    System.out.println("INPUT NEW DEFINITION: ");
-                    String newSlangDefinition = myReader.nextLine();
-                    System.out.println("CONFIRM TO ADD (Y / N):");
-                    String confirm = myReader.nextLine();
-                    if (confirm.equalsIgnoreCase("y") || confirm.equalsIgnoreCase("yes")) {
-                        addNewDefinition(slangWord, newSlangDefinition);
-                        updateFileData();
-                        System.out.println("SUCCESSFULLY ADD NEW DEFINITION");
-                    }
-                    else System.out.println("PLEASE INPUT Y / N");
-                }
-                else {
-                    System.out.println("INVALID INPUT");
-                }
-            }
-            else {
-                System.out.println("PLEASE PRESS ONLY 1 OR 2");
-                editSlangWord(slangWord);
-                return;
-            }
-        }
-        else
-            System.out.println("DONT HAVE SLANG WORD " + slangWord + " ON DATABASE");
-    }
-
     public void changeSlangWordKey(String oldSlangWordKey, String newSlangWordkey) {
         ArrayList<String> slangDefinitions= new ArrayList<>(m_slangDic.get(oldSlangWordKey));
         m_slangDic.remove(oldSlangWordKey);
@@ -214,6 +176,93 @@ public class SlangDictionary {
         slangDefinitions.add(newSlangDefinition);
     }
 
+    public void editSlangWord(String slangWord) {
+        if (m_slangDic.containsKey(slangWord)) {
+            ArrayList<String> slangDefinitions = m_slangDic.get(slangWord);
+            System.out.println("ORIGINAL SLANG WORD " + slangWord + " HAS " + slangDefinitions.size() + " MEANINGS");
+            for (int i = 0; i < slangDefinitions.size(); i++)
+                System.out.println((i + 1) + ".\t" + slangDefinitions.get(i));
+
+            System.out.println("PRESS 0: TO EXIT EDIT THIS SLANG WORD");
+            System.out.println("PRESS 1: TO EDIT SLANG WORD");
+            System.out.println("PRESS 2: TO EDIT MEANING");
+            Scanner myReader = new Scanner(System.in);
+            int choose = myReader.nextInt();
+            myReader.nextLine();
+
+            if (choose == 0) {
+                System.out.println("PRESS ENTER TO EXIT EDIT THIS SLANG WORD");
+                myReader.nextLine();
+            }
+            else if (choose == 1) {
+                System.out.print("INPUT NEW SLANG WORD KEY: ");
+                String newSlangWord = myReader.nextLine();
+                boolean confirm = confirmOption();
+                if (confirm) {
+                    changeSlangWordKey(slangWord, newSlangWord);
+                    updateFileData();
+                    System.out.println("SUCCESSFULLY CHANGE SLANG WORD");
+                }
+                else {
+                    System.out.println("YOU DONT WANT TO CHANGE. PRESS ENTER");
+                    myReader.nextLine();
+                    editSlangWord(slangWord);
+                }
+            }
+            else if (choose == 2) {
+                if (slangDefinitions.size() == 1)
+                    System.out.println("PRESS 1 TO EDIT DEFINITION");
+                else System.out.println("PRESS FROM 1 TO " + slangDefinitions.size() + " TO EDIT CORRESPONDING DEFINITION");
+                System.out.println("PRESS " + (slangDefinitions.size() + 1) + " TO ADD NEW DEFINITION");
+                int num = myReader.nextInt() - 1;
+                myReader.nextLine();
+
+                if (num < slangDefinitions.size()) {
+                    System.out.println("INPUT NEW DEFINITION: ");
+                    String newSlangDefinition= myReader.nextLine();
+                    boolean confirm = confirmOption();
+                    if (confirm) {
+                        changeDefinition(slangWord, num, newSlangDefinition);
+                        updateFileData();
+                        System.out.println("SUCCESSFULLY CHANGE SLANG DEFINITION");
+                    }
+                    else {
+                        System.out.println("YOU DONT WANT TO CHANGE. PRESS ENTER");
+                        myReader.nextLine();
+                        editSlangWord(slangWord);
+                    }
+                }
+                else if (num == slangDefinitions.size()) {
+                    System.out.println("INPUT NEW DEFINITION: ");
+                    String newSlangDefinition = myReader.nextLine();
+                    boolean confirm = confirmOption();
+                    if (confirm) {
+                        addNewDefinition(slangWord, newSlangDefinition);
+                        updateFileData();
+                        System.out.println("SUCCESSFULLY ADD NEW DEFINITION");
+                    }
+                    else {
+                        System.out.println("YOU DONT WANT TO CHANGE. PRESS ENTER");
+                        myReader.nextLine();
+                        editSlangWord(slangWord);
+                    }
+                }
+                else {
+                    System.out.println("INVALID INPUT");
+                }
+            }
+            else {
+                System.out.println("PLEASE PRESS VALID VALUE");
+                System.out.println("PLEASE ENTER TO INPUT AGAIN");
+                myReader.nextLine();
+                editSlangWord(slangWord);
+                return;
+            }
+        }
+        else
+            System.out.println("DONT HAVE SLANG WORD " + slangWord + " ON DATABASE");
+    }
+
     //Option 06: Delete slangWord
     public void deleteSlangWord(String slangWord) {
         if (m_slangDic.containsKey(slangWord)) {
@@ -222,37 +271,50 @@ public class SlangDictionary {
             for (int i = 0; i < slangDefinitions.size(); i++)
                 System.out.println((i + 1) + ".\t" + slangDefinitions.get(i));
 
+            System.out.println("PRESS 0: TO EXIT DELETE THIS SLANG WORD");
             System.out.println("PRESS 1: TO DELETE SLANG WORD");
             System.out.println("PRESS 2: TO DELETE MEANING");
             Scanner myReader = new Scanner(System.in);
             int choose = myReader.nextInt();
             myReader.nextLine();
 
-            if (choose == 1) {
-                System.out.print("CONFIRM TO DELETE (Y / N):");
-                String confirm = myReader.nextLine();
-                if (confirm.equalsIgnoreCase("y") || confirm.equalsIgnoreCase("yes")) {
+            if (choose == 0) {
+                System.out.println("PRESS ENTER TO EXIT DELETE THIS SLANG WORD");
+                myReader.nextLine();
+            }
+            else if (choose == 1) {
+                boolean confirm = confirmOption();
+                if (confirm) {
                     m_slangDic.remove(slangWord);
                     updateFileData();
                     System.out.println("SUCCESSFULLY DELETE SLANG WORD");
                 }
-                else System.out.println("PLEASE INPUT Y / N");
+                else {
+                    System.out.println("YOU DONT WANT TO DELETE. PRESS ENTER");
+                    myReader.nextLine();
+                    deleteSlangWord(slangWord);
+                }
             }
             else if (choose == 2) {
                 if (slangDefinitions.size() == 1)
                     System.out.println("PRESS 1 TO DELETE DEFINITION");
-                else System.out.println("PRESS FROM 1 TO " + slangDefinitions.size() + " TO DELETE DEFINITION");
+                else System.out.println("PRESS FROM 1 TO " + slangDefinitions.size() + " TO DELETE CORRESPONDING DEFINITION");
                 int num = myReader.nextInt() - 1;
                 myReader.nextLine();
                 if (num < slangDefinitions.size()) {
-                    System.out.println("CONFIRM TO DELETE (Y / N):");
-                    String confirm = myReader.nextLine();
-                    if (confirm.equalsIgnoreCase("y") || confirm.equalsIgnoreCase("yes")) {
-                        slangDefinitions.remove(num);
+                    boolean confirm = confirmOption();
+                    if (confirm) {
+                        if (num == slangDefinitions.size())
+                            m_slangDic.remove(slangWord);
+                        else slangDefinitions.remove(num);
                         updateFileData();
                         System.out.println("SUCCESSFULLY CHANGE SLANG DEFINITION");
                     }
-                    else System.out.println("PLEASE INPUT Y / N");
+                    else {
+                        System.out.println("YOU DONT WANT TO DELETE. PRESS ENTER");
+                        myReader.nextLine();
+                        deleteSlangWord(slangWord);
+                    }
                 }
                 else  {
                     System.out.println("INVALID INPUT");
@@ -271,6 +333,7 @@ public class SlangDictionary {
     public void resetToOriginalData() {
         m_slangDic.clear();
         SlangFileHandling.readFromFile(file_original, m_slangDic);
+        FileHandling.writeToFile(file_history, "");
         updateFileData();
         System.out.println("SUCCESSFULLY RESET ORIGINAL DATABASE");
     }
@@ -326,16 +389,17 @@ public class SlangDictionary {
         String slangWordAnswer = getRandomSlangWord();
         String slangDefinitionAnswer = getRandomSlangDefinition(slangWordAnswer);
 
+        int numAnswer = 4;
         ArrayList<String> listAnswer = new ArrayList<>();
 
         if (type == 1) {
             listAnswer.add(slangDefinitionAnswer);
-            for (int i = 1; i <= 3; i++)
+            for (int i = 1; i < numAnswer; i++)
                 listAnswer.add(getRandomSlangDefinition(getRandomSlangWord()));
         }
         else {
             listAnswer.add(slangWordAnswer);
-            for (int i = 1; i <= 3; i++)
+            for (int i = 1; i < numAnswer; i++)
                 listAnswer.add(getRandomSlangWord());
         }
         Collections.shuffle(listAnswer);
@@ -343,22 +407,28 @@ public class SlangDictionary {
         if (type == 1)
             System.out.println("SLANG WORD: " + slangWordAnswer + " MEAN?");
         else System.out.println("DEFINITION: " + slangDefinitionAnswer + " FOR SLANG WORD?");
-        System.out.println("CHOOSE CORRECT ANSWER NUMBER ANSWER");
+
+        System.out.println("CHOOSE CORRECT ANSWER NUMBER ANSWER BELOW");
         showQuizAnswer(listAnswer);
+
         boolean con = true;
         while (con) {
             boolean correct;
             if (type == 1) correct = checkAnswer(listAnswer.indexOf(slangDefinitionAnswer));
             else correct = checkAnswer(listAnswer.indexOf(slangWordAnswer));
             if (!correct) {
-                System.out.println("ARE YOU WANT ANSWER AGAIN (Y / N): ");
+                System.out.print("ARE YOU WANT ANSWER AGAIN (Y / N): ");
                 Scanner myReader = new Scanner(System.in);
                 String confirm = myReader.nextLine();
                 if (confirm.equalsIgnoreCase("y") || confirm.equalsIgnoreCase("yes"))
                     con = true;
                 else if (confirm.equalsIgnoreCase("n") || confirm.equalsIgnoreCase("no"))
                     con = false;
-                else System.out.println("PLEASE INPUT Y / N");
+                else {
+                    System.out.println("PLEASE PRESS Y / N");
+                    System.out.println("PRESS ENTER TO INPUT AGAIN");
+                    con = true;
+                }
             }
             else con = false;
         }
